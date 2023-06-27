8d2eb5ab33898d81e37fd56e8309e97f47b35a22
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-27 21:21:17 +0530

    setting_up_app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<App></App>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
