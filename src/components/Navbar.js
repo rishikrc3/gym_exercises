f089f82098b659579ef7429947ac67bd5590f632
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-28 00:57:24 +0530

    hero_banner_updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 
-//constants import
 import Logo from "../assets/images/Logo.png";
+
 const Navbar = () => (
   <Stack
     direction="row"
     justifyContent="space-around"
     sx={{
-      gap: { sm: "300px", xs: "20px" },
+      gap: { sm: "123px", xs: "40px" },
       mt: { sm: "32px", xs: "20px" },
       justifyContent: "none",
     }}
