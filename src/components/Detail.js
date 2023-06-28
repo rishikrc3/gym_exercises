4af16443406431698e21be86320c3c18b0680024
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-28 21:27:17 +0530

    image_added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
 
-const Detail = () => {
-  return <div>Detail</div>;
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+    </Stack>
+  );
 };
 
 export default Detail;
