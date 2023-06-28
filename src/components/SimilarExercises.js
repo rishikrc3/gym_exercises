924ebce4f221e0fbc1f9b8f325bc19e6150ce2a2
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-29 01:42:10 +0530

    rendering_footer

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -24,6 +24,29 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
           <Loader />
         )}
       </Stack>
+      <Typography
+        sx={{
+          fontSize: { lg: "44px", xs: "25px" },
+          ml: "20px",
+          mt: { lg: "100px", xs: "60px" },
+        }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Similar{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          Equipment
+        </span>{" "}
+        exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: 2, position: "relative" }}>
+        {equipmentExercises.length !== 0 ? (
+          <HoritzontalScrollbar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
     </Box>
   );
 };
